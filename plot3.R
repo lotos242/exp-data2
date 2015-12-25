6e2936439d09707af591846d11b1aa1136fffad5
lotos242 <lotos242@users.noreply.github.com>
2015-12-25 12:53:28 -0500

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(ggplot2)
+setwd("C:/Users/solot/Downloads/exdata-data-NEI_data")
+scc <- readRDS("summarySCC_PM25.rds")
+balt <- filter(scc, fips == 24510)
+remove(scc)
+bytype <- aggregate(Emissions ~ type + year, data = balt, FUN = sum)
+bytype$type <- factor(bytype$type)
+png("plot3.png", width = 480, height = 480)
+qplot(year, Emissions, data = bytype, facets = . ~ type, geom = "line", 
+      main = "Total Emissions by Type for Baltimore City")
+dev.off()
