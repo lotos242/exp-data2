9d5a5045d46d50dba27bb0ec9f9605a0cf2d0340
lotos242 <lotos242@users.noreply.github.com>
2015-12-25 12:58:42 -0500

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -1,6 +1,6 @@
 library(dplyr)
 library(ggplot2)
-setwd("C:/Users/solot/Downloads/exdata-data-NEI_data")
+setwd("/Users/Maria/Documents/rcode/exdata-data-NEI_data")
 scc <- readRDS("summarySCC_PM25.rds")
 scode <- readRDS("Source_Classification_Code.rds")
 onroad <- filter(scode, grepl("On-Road", EI.Sector, ignore.case = FALSE))
