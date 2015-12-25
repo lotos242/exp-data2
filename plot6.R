b1b5880e7d3769d4ab52f1be30a7a19678b94969
lotos242 <lotos242@users.noreply.github.com>
2015-12-25 13:08:30 -0500

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -3,10 +3,12 @@ library(ggplot2)
 setwd("/Users/Maria/Documents/rcode/exdata-data-NEI_data")
 scc <- readRDS("summarySCC_PM25.rds")
 scode <- readRDS("Source_Classification_Code.rds")
+## Use similar methods but include the Los Angeles data
 onroad <- filter(scode, grepl("On-Road", EI.Sector, ignore.case = FALSE))
 balt_la <- filter(scc, fips == "24510" | fips == "06037")
 codes <- onroad$SCC
 balt_laroad <- filter(balt_la, SCC %in% codes)
+## Include the fips data with aggregate this time to include both L.A. and Balt.
 final <- aggregate(Emissions ~ fips + year, data = balt_laroad, FUN = sum)
 png("plot6.png", width = 480, height = 480)
 qplot(year, Emissions, data = final, facets = . ~ fips, geom = "line",
