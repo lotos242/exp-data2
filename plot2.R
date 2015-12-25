44a0dc77ea81471fc3b864644d2fa021b0462146
lotos242 <lotos242@users.noreply.github.com>
2015-12-25 13:01:44 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,7 +1,9 @@
 setwd("/Users/Maria/Documents/rcode/exdata-data-NEI_data")
 library(dplyr)
 scc <- readRDS("summarySCC_PM25.rds")
+## Create a new data frame with just Baltimore data
 balt <- filter(scc, fips == 24510)
+## Use the aggregate function as in plot1.R
 baltagg <- aggregate(Emissions ~ year, data = balt, sum)
 png("plot2.png")
 plot(baltagg$year, baltagg$Emissions, xlab = "Year", 
