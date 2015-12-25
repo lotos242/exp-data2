600a0ce8d04e4785bc57d82779d2908a80b9509f
lotos242 <lotos242@users.noreply.github.com>
2015-12-25 13:00:27 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,7 @@
 library(dplyr)
 setwd("/Users/Maria/Documents/rcode/exdata-data-NEI_data")
 scc <- readRDS("summarySCC_PM25.rds")
+## Use the aggregate function to group the data by year and Emissions sums
 mydf <- aggregate(Emissions ~ year, data = scc, sum)
 png("plot1.png", width = 480, height = 480)
 plot(mydf$year, mydf$Emissions, type = "l")
