c6152b46eb00542cad633a0f01cbd62f19fc9227
lotos242 <lotos242@users.noreply.github.com>
2015-12-25 13:02:50 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -4,6 +4,7 @@ setwd("/Users/Maria/Documents/rcode/exdata-data-NEI_data")
 scc <- readRDS("summarySCC_PM25.rds")
 balt <- filter(scc, fips == 24510)
 remove(scc)
+## Use the aggregate function but this time include both type and year in the resulting data frame
 bytype <- aggregate(Emissions ~ type + year, data = balt, FUN = sum)
 bytype$type <- factor(bytype$type)
 png("plot3.png", width = 480, height = 480)
